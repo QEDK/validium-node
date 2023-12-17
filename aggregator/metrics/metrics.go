44f30ada254422b9fc27ff6064406434790fc742
Arpit Temani <temaniarpit27@gmail.com>
2023-12-17 16:40:32 +0530

    debug statement

--- aggregator/metrics/metrics.go
+++ aggregator/metrics/metrics.go
@@ -13,6 +13,7 @@ const (
 
 // Register the metrics for the sequencer package.
 func Register() {
+	fmt.Println("----- debug")
 	gauges := []prometheus.GaugeOpts{
 		{
 			Name: currentConnectedProversName,
